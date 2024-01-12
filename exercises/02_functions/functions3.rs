5156910984fe217a096a082420d5406ab574b1c8
Thomas Genoud <thomy672005@gmail.com>
2024-01-12 22:58:12 +0000

    Debut Rust

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    call_me();
+    call_me(4);
 }
 
-fn call_me(num: u32) {
+fn call_me(num:u32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
