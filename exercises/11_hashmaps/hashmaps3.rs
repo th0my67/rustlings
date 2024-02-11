57a852a3aa8a3dae829d1f74a88bd86ff0f61ca8
Thomas Genoud <thomy672005@gmail.com>
2024-02-11 08:56:51 +0000

    RUsT

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -39,7 +39,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        
+        scores.entry(team_1_name).and_modify(|team| {*team.goals_conceded += team_2_score; *team.goals_scored}).or_insert(Team {goals_scored: team_1_score, goals_conceded: team_2_score})
+        scores.entry(team_2_name).and_modify().or_insert(Team {goals_scored: team_2_score, goals_conceded: team_1_score});
     }
     scores
 }
